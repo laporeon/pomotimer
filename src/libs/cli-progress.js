25f29f617bac8785a560dcf1436de1b243fe5222
laporeon <34722707+laporeon@users.noreply.github.com>
2023-11-09 14:56:36 -0300

    feat: create break progress bar

--- src/libs/cli-progress.js
+++ src/libs/cli-progress.js
@@ -1,17 +1,17 @@
 import chalk from 'chalk';
 import cliProgress from 'cli-progress';
 
-export const progressBar = new cliProgress.SingleBar({
+const focusProgressBar = new cliProgress.SingleBar({
   format:
-    'Focus ' +
+    'Focus Time ' +
     chalk.cyan('{bar}') +
-    ' {percentage}%  {duration_formatted}/{focus_time_formatted}',
+    ' {percentage}%  {duration_formatted}/{time_formatted}',
   barCompleteChar: '\u2588',
   barIncompleteChar: '\u2591',
   hideCursor: true,
   stopOnComplete: true,
   synchronousUpdate: true,
-  focus_time_formatted: '00:00',
+  time_formatted: '00:00',
   formatTime: time => {
     const minutes = Math.floor((time % 3600) / 60);
     const seconds = time % 60;
@@ -21,3 +21,26 @@ export const progressBar = new cliProgress.SingleBar({
     )}`;
   },
 });
+
+const breakProgressBar = new cliProgress.SingleBar({
+  format:
+    'Break Time ' +
+    chalk.cyan('{bar}') +
+    ' {percentage}%  {duration_formatted}/{time_formatted}',
+  barCompleteChar: '\u2588',
+  barIncompleteChar: '\u2591',
+  hideCursor: true,
+  stopOnComplete: true,
+  synchronousUpdate: true,
+  time_formatted: '00:00',
+  formatTime: time => {
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
+      2,
+      '0',
+    )}`;
+  },
+});
+
+export { focusProgressBar, breakProgressBar };
