a2ebaf86320715fa75d16db7e291680414e650b2
laporeon <34722707+laporeon@users.noreply.github.com>
2023-11-10 11:57:43 -0300

    refactor: reduce exports to a single bar creation

--- src/libs/cli-progress.js
+++ src/libs/cli-progress.js
@@ -1,9 +1,9 @@
 import chalk from 'chalk';
 import cliProgress from 'cli-progress';
 
-const focusProgressBar = new cliProgress.SingleBar({
+export const progressBar = new cliProgress.SingleBar({
   format:
-    'Focus Time ' +
+    '{type} Time ' +
     chalk.cyan('{bar}') +
     ' {percentage}%  {duration_formatted}/{time_formatted}',
   barCompleteChar: '\u2588',
@@ -12,6 +12,7 @@ const focusProgressBar = new cliProgress.SingleBar({
   stopOnComplete: true,
   synchronousUpdate: true,
   time_formatted: '00:00',
+  type: '',
   formatTime: time => {
     const minutes = Math.floor((time % 3600) / 60);
     const seconds = time % 60;
@@ -21,26 +22,3 @@ const focusProgressBar = new cliProgress.SingleBar({
     )}`;
   },
 });
-
-const breakProgressBar = new cliProgress.SingleBar({
-  format:
-    'Break Time ' +
-    chalk.cyan('{bar}') +
-    ' {percentage}%  {duration_formatted}/{time_formatted}',
-  barCompleteChar: '\u2588',
-  barIncompleteChar: '\u2591',
-  hideCursor: true,
-  stopOnComplete: true,
-  synchronousUpdate: true,
-  time_formatted: '00:00',
-  formatTime: time => {
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
-      2,
-      '0',
-    )}`;
-  },
-});
-
-export { focusProgressBar, breakProgressBar };
