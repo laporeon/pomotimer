934f512f9aac364ff6b8d6eb2569cd8043b418ff
laporeon <34722707+laporeon@users.noreply.github.com>
2023-11-20 15:45:07 -0300

    feat: add description as input option

--- src/index.js
+++ src/index.js
@@ -9,18 +9,24 @@ program
   .option('-f, --focus <value>', 'Focus time in minutes', '25')
   .option(
     '-t, --title <value>',
-    'Customize the title for your pomodoro session',
+    'Customize the title for your Pomodoro session.',
     'Pomotimer',
   )
+  .option(
+    '-d, --description <value>',
+    'Customize the description for your notification.',
+    'Congratulations! Session completed.',
+  )
+
   .action(options => {
-    const { focus, title } = options;
+    const { focus, title, description } = options;
 
     if (Number.isNaN(+focus) || !Number.isInteger(+focus))
       throw new Error(
         'Please, enter a valid value for your focus session. e.g: 10',
       );
 
-    const pomodoro = new Pomodoro(title, focus);
+    const pomodoro = new Pomodoro(title, focus, description);
 
     pomodoro.init();
   });
