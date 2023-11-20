af160d4de0143504404dbacd9133ddd577a7c5a7
laporeon <34722707+laporeon@users.noreply.github.com>
2023-11-20 17:19:10 -0300

    refactor: change bar colors

--- src/libs/cli-progress.js
+++ src/libs/cli-progress.js
@@ -3,9 +3,10 @@ import cliProgress from 'cli-progress';
 
 export const progressBar = new cliProgress.SingleBar({
   format:
-    '{type} Time [' +
+    chalk.yellow('[') +
     chalk.hex('fc5d3d').bold('{bar}') +
-    '] {percentage}%  {elapsed_time}/{total_time}',
+    chalk.yellow(']') +
+    ' {percentage}%  {elapsed_time}/{total_time}',
   barCompleteChar: '\u2588',
   barIncompleteChar: '\u2591',
   hideCursor: true,
@@ -13,5 +14,4 @@ export const progressBar = new cliProgress.SingleBar({
   synchronousUpdate: true,
   elapsed_time: '00:00',
   total_time: '00:00',
-  type: '',
 });
