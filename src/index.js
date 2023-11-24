05bb3ac1136d2a6529df41f01324ee3ca552018a
laporeon <34722707+laporeon@users.noreply.github.com>
2023-11-24 14:36:08 -0300

    feat: add pause and cycles options

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ program.name('pomotimer').description('A Pomodoro CLI timer.').version('1.0.0');
 program
   .option('-f, --focus <value>', 'Focus time in minutes', '25')
   .option('-p, --pause <value>', 'Break time in minutes', '5')
+  .option('-c, --cycles <value>', 'How many cycles you want do do', '4')
   .option('-t, --title <value>', 'Customize Pomodoro title.', 'Pomotimer')
   .option(
     '-d, --description <value>',
@@ -18,13 +19,19 @@ program
   .option('-s, --style <value>', 'Customize CLI text color.', 'morning')
 
   .action(options => {
-    console.log({ options });
-    const { focus, pause, title, description, style } = options;
+    const { focus, pause, cycles, title, description, style } = options;
 
-    isValidNumber(focus);
+    isValidNumber([focus, pause, cycles]);
     isValidGradientStyle(style);
 
-    const pomodoro = new Pomodoro(title, focus, pause, description, style);
+    const pomodoro = new Pomodoro(
+      title,
+      focus,
+      pause,
+      cycles,
+      description,
+      style,
+    );
 
     pomodoro.init();
   })
@@ -33,7 +40,8 @@ program
     `\nExamples:
     $ pomotimer -t "Studying JavaScript"
     $ pomotimer -s "summer"
-    $ pomotimer -f 5 -t "Reading" -d "Finished" -s "rainbow"`,
+    $ pomotimer -f 15 -p 5 -c 2
+    $ pomotimer -f 30 -p 10 -c 5 -t "Reading" -d "Finished" -s "rainbow"`,
   )
   .showSuggestionAfterError();
 
