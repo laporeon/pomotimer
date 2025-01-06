30be45be9c9fa535d304fe83f2630a5fbb2d23c7
laporeon <175569928+daspeon@users.noreply.github.com>
2025-01-06 18:39:02 -0300

    fix: remove unused validator for style option

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,26 +1,3 @@
-export const isValidGradientStyle = style => {
-  const styles = [
-    'cristal',
-    'teen',
-    'mind',
-    'morning',
-    'vice',
-    'passion',
-    'fruit',
-    'instagram',
-    'atlas',
-    'retro',
-    'summer',
-    'pastel',
-    'rainbow',
-  ];
-
-  if (!styles.includes(style))
-    throw new Error(
-      `Please, choose a valid option for stylization: ${styles.join(', ')}.`,
-    );
-};
-
 export const isValidNumber = values => {
   values.forEach(value => {
     if (Number.isNaN(+value) || !Number.isInteger(+value))
