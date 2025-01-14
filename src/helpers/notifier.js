4613a02dc7dfad3f6073e61aa93351d5577425b5
laporeon <175569928+daspeon@users.noreply.github.com>
2025-01-14 16:43:50 -0300

    feat: implement sound notification

--- src/helpers/notifier.js
+++ src/helpers/notifier.js
@@ -3,13 +3,25 @@ import { fileURLToPath } from 'node:url';
 
 import notifier from 'node-notifier';
 
+import { playSound } from './playSound.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const notificationAlert = (title, description) => {
-  return notifier.notify({
-    title,
-    message: description,
-    icon: path.join(__dirname, '../../assets/images/icon.png'),
-  });
+export const notificationAlert = (title, description, status) => {
+  return notifier.notify(
+    {
+      title,
+      message: description,
+      icon: path.join(__dirname, '../../assets/images/icon.png'),
+      sound: false,
+    },
+    err => {
+      if (err) {
+        console.error('Erro ao enviar a notificação:', err.message);
+      } else {
+        playSound(status);
+      }
+    },
+  );
 };
