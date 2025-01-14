822f41ca8e36856904daea9d05b4f109b95b8d96
laporeon <175569928+daspeon@users.noreply.github.com>
2025-01-14 16:44:26 -0300

    feat: create function to play sounds at notifications
    Signed-off-by: laporeon <175569928+daspeon@users.noreply.github.com>

---
+++ src/helpers/playSound.js
@@ -0,0 +1,30 @@
+import { exec } from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const platform = process.platform;
+
+export const playSound = (status = 'completed') => {
+  let soundPath = path.join(__dirname, '../../assets/sounds/alarm');
+
+  if (status !== 'completed') {
+    soundPath = path.join(__dirname, '../../assets/sounds/ding');
+  }
+
+  const linuxCmd = `paplay ${soundPath}.ogg`;
+  const windowsCmd = `wscript ${path.join(
+    __dirname,
+    '../../assets/sounds/forWindows.vbs',
+  )} ${soundPath}.mp3`;
+
+  if (platform === 'linux') {
+    return exec(linuxCmd);
+  }
+
+  if (platform === 'win32') {
+    return exec(windowsCmd);
+  }
+};
